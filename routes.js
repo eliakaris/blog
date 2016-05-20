a61687a254efe11f31d9739a54d7523dfeb69d29
eliakaris <eliakaris@hotmail.com>
2016-05-20 07:29:08 -0700

    Add in new redirect for mpgmeter

--- routes.js
+++ routes.js
@@ -2,7 +2,10 @@ var home = require('./controllers/home'),
     about = require('./controllers/about'),
     blog = require('./controllers/blog');
 
-exports.init = function(app) {
+exports.init = function (app) {
+  app.get('/mpgmeter', function (req, res) {
+      res.redirect('https://www.microsoft.com/en-us/store/apps/mpg-meter/9nblggh1b66t');
+  });
   app.get('/', home.index);
   app.get('/about', about.index);
   app.get('/blog', blog.index);
