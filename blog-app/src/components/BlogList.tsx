6d80ff518aec3497750ae434fbdd7c69cd0c7450
Elia Karagiannis <eliakaris@hotmail.com>
2017-12-16 10:17:04 -0800

    Remove dispatch from blog list

--- blog-app/src/components/BlogList.tsx
+++ blog-app/src/components/BlogList.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
-import { Dispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { BlogEntryData } from '../types/BlogEntry';
 
 export interface Props {
   blogEntries: BlogEntryData[];
-  dispatch: Dispatch<{}>;
 }
 
 class BlogList extends React.Component<Props> {
