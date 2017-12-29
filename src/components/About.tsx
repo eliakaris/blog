e39af708494e687c08aa29a20c9aa1518f162540
Elia Karagiannis <ekaragiannis@pinterest.com>
2017-12-28 19:15:20 -0800

    Fix the about page and update the main picture

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 
+import './About.css';
+
 function About() {
   return (
     <div>
-      <img className="me" src="/img/me.jpg" width="432" height="432" />
+      <img className="me" src="/img/me2.jpg" />
       <h2>About me</h2>
       <p>Hey there. My name is Elia Karagiannis and I live in Seattle, Washington.</p>
 
@@ -16,24 +18,6 @@ function About() {
       <h2>Hobbies</h2>
       <p><a href="http://github.com/eliakaris">programming</a>, 
       <a href="http://www.bing.com/search?q=rams+football">sports</a></p>
-
-      <h2>Contact</h2>
-      <a href="mailto:eliakaris@hotmail.com" rel="author">eliakaris@hotmail.com</a>,
-        <a href="http://twitter.com/eliakaris">@eliakaris</a>,
-        <a href="http://www.linkedin.com/in/eliakaris">LinkedIn</a>,
-        <a href="http://www.facebook.com/eliakaris">Facebook</a>
-      <p>
-        <a
-          href="https://twitter.com/eliakaris"
-          className="twitter-follow-button"
-          data-show-count="true"
-          data-lang="en"
-          data-show-screen-name="true"
-          data-size="large"
-        >Follow @eliakaris
-        </a>
-      </p>
-
     </div>
   );
 }
