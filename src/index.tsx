5e8e7b9f92b89d9bad7f3cf9b7e1bc074e7b0ac9
Elia Karagiannis <ekaragiannis@pinterest.com>
2018-03-20 22:21:37 -0700

    Add in client side app insights

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,19 @@ import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
+import { AppInsights } from "applicationinsights-js"
 
 import 'startbootstrap-clean-blog/vendor/bootstrap/css/bootstrap.min.css';
 import 'startbootstrap-clean-blog/vendor/font-awesome/css/font-awesome.min.css';
 import 'startbootstrap-clean-blog/css/clean-blog.min.css';
 
 import './index.css';
+
+if (process.env.REACT_APP_INSIGHTS_KEY) {
+  AppInsights 
+    && AppInsights.downloadAndSetup 
+    && AppInsights.downloadAndSetup({ instrumentationKey: process.env.REACT_APP_INSIGHTS_KEY });
+}
 
 ReactDOM.render(
   <Provider store={configureStore()}>
