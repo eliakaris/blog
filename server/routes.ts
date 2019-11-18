5f6c977e32fa86a0f695542a3a44d4d34ebb41b5
Elia Karagiannis <ekaragiannis@pinterest.com>
2019-11-17 22:25:37 -0800

    Remove unneeded routes from server

--- server/routes.ts
+++ server/routes.ts
@@ -1,19 +1,9 @@
 import * as express from 'express';
-import * as path from 'path';
 
 export function init (app: express.Application) {
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
-  });
-  app.use(express.static(path.join(__dirname, '../')));
-  app.use(express.static(path.join(__dirname, '../../node_modules/bootstrap/dist')));
-  app.use((req, res, next) => {
-    if (!req.path.toLocaleLowerCase().startsWith('/api')) {
-      res.sendFile(path.resolve(__dirname, '../index.html'));
-    } else {
-      next();
-    }
   });
 }
