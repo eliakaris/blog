63b86cee7e7685759592f8bf9d4f6cab199beed3
Elia Karagiannis <ekaragiannis@pinterest.com>
2019-11-20 15:06:20 -0800

    Add in revert proxy

--- docker-clear.bat
+++ docker-clear.bat
@@ -1,4 +1,6 @@
 @echo off
 FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker rm %%i
 FOR /f "tokens=*" %%i IN ('docker images eliakaris/blog -q') DO docker rmi -f %%i
+FOR /f "tokens=*" %%i IN ('docker images eliakaris/server -q') DO docker rmi -f %%i
+FOR /f "tokens=*" %%i IN ('docker images eliakaris/prod-proxy -q') DO docker rmi -f %%i
 FOR /f "tokens=*" %%i IN ('docker images --filter "dangling=true" -q') DO docker rmi -f %%i
